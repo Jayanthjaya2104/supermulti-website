662019f143c1166504d2880bc8d3a0eaeba40065
Jayanthjaya2104 <138043553+Jayanthjaya2104@users.noreply.github.com>
2023-11-09 19:04:24 +0530

    Update script.js

--- snake-game/script.js
+++ snake-game/script.js
@@ -94,5 +94,5 @@ const initGame = () => {
 
 
 changeFoodPosition();
-setIntervalId = setInterval(initGame, 201);
+setIntervalId = setInterval(initGame, 301);
 document.addEventListener("keydown", changeDirection);
